b5629fbd7e394356b22bf5fbe69ece691c84d1f2
Pushpak Vattam <vattampushpak243@gmail.com>
2024-04-08 07:51:53 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- vars.tf
+++ vars.tf
@@ -3,10 +3,10 @@ variable "env" {}
 variable "subnets" {}
 variable "sg_cidrs" {}
 variable "vpc_id" {}
-variable "rds_allocated_storage" {}
-variable "rds_engine" {}
-variable "rds_engine_version" {}
-variable "rds_instance_class" {}
+variable "allocated_storage" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
 variable "kms" {}
 variable "parameter_group_family" {}
 
